c1af92bb76b8102001d4bb67fb38262cfd948b11
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2019-01-11 14:39:10 -0600

    minor (pep 8)

--- scripts/download.py
+++ scripts/download.py
@@ -23,15 +23,15 @@ for link in tqdm(links):
         with open(filename + '.download', "wb") as f:
             response = requests.get(link, stream=True)
             total_length = int(response.headers.get('content-length'))
-            chunk_size = 2**20
+            chunk_size = 1 << 20
             if total_length is None: # no content length header
                 f.write(response.content)
             else:
                 for data in tqdm(
                     response.iter_content(chunk_size=chunk_size), 
-                    desc=filename +' (MiB)',
-                    total=(total_length+chunk_size-1)//chunk_size):
+                    desc=filename + ' (MiB)',
+                    total=(total_length + chunk_size - 1) // chunk_size):
                     f.write(data)
-        if total_length == os.path.get_size(filename+'.download'):
+        if total_length == os.path.get_size(filename + '.download'):
             os.rename(filename + '.download', filename)
 
