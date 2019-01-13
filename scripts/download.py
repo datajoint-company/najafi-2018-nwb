67d121df69e495047d1d3b2e41ee9644d9713e3b
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2019-01-13 17:02:45 -0600

    simplify download.py

--- scripts/download.py
+++ scripts/download.py
@@ -12,26 +12,22 @@ links = [r.group('link') for r in (
     re.search(
         r'"(?P<link>http(s)?://[\w./-]+FN_dataSharing.tgz-\w+)".*Download',  line)
     for line in request.text.split('\n')) if r]
-print('Download links:', *links, sep='\n')
+print('Download links:', *links, '\n', sep='\n')
 
 # download files from links
 for link in tqdm(links):
     filename = os.path.join('data',  link.split('/')[-1])
-    if os.path.isfile(filename):
-        print(filename, '- done!')
-    else:
+    if not os.path.isfile(filename):
         with open(filename + '.download', "wb") as f:
             response = requests.get(link, stream=True)
             total_length = int(response.headers.get('content-length'))
             chunk_size = 1 << 20
-            if total_length is None: # no content length header
-                f.write(response.content)
-            else:
-                for data in tqdm(
+            for data in tqdm(
                     response.iter_content(chunk_size=chunk_size), 
                     desc=filename + ' (MiB)',
+                    smoothing = 0.01,
                     total=(total_length + chunk_size - 1) // chunk_size):
-                    f.write(data)
+                f.write(data)
         if total_length == os.path.getsize(filename + '.download'):
             os.rename(filename + '.download', filename)
 
